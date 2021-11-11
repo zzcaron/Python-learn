d313410bcb18344278e72ea51d28e9b8e6b99769
Aron Clayderman <30361681+zzcaron@users.noreply.github.com>
2021-11-11 18:21:55 +0800

    Add files via upload

---
+++ first_python & 2.py
@@ -0,0 +1,41 @@
+# 基本资料形态&变数
+
+#字串
+"小白你好"
+
+#数字
+70
+-87
+160.55555
+
+
+
+#布林值
+True
+False
+
+name ="小白"
+age =87
+is_male =True
+
+# 1.变数的名称只能是英文or数字or_的组合
+# 2.变数的开头不可以是数字
+
+
+
+
+# 清楚指令 windows cls    Mac clear
+
+print("有一个人叫" + name)
+print(name + "今年87岁")
+print(name + "身高187公分")
+print(name + "讨厌自己87岁")
+
+name = "小绿"
+print(name)
+name = "小兰"
+print(name)
+name = "50"
+print(name)
+name = True
+print(name)
